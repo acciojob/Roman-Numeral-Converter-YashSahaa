91708596c14c2fe65bb4a9d2738aa0ab23135626
YashSahaa <136882182+YashSahaa@users.noreply.github.com>
2024-05-08 16:44:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -10,11 +10,33 @@ function convertToRoman(num) {
     };
 
   //your code here
+	let result = "";
+
+  for (let key in obj) {
+		let sym = obj[key][0]
+		let val = obj[key][1]
+	    let count = Math.floor(num / val);
+	
+	    if (count >= 1) {
+	      result += sym.repeat(count);
+	      num -= val * count;
+	    }
+	    if (key % 2 === 0 && (key+2)<=6 &&num >=val-obj[key + 2][1]) {
+	      result += obj[key + 2][0] + sym;
+	      num -= obj[key + 2][1] - val;
+	    }
+	    else if (key % 2 === 1 && (key+1)<=6 &&num >=val-obj[key + 1][1]) {
+	      result += obj[key + 1][0] + sym;
+	      num -= obj[key + 1][1] - val;
+	    }
+  }
+
+  return result;
 
 }
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
-// console.log(convertToRoman(36));
+ console.log(convertToRoman(36));
 
 
 
